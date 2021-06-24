f25a615565086d869999bac64f0a96ec28612d1c
Mina Nagy Zaki <mnzaki@gmail.com>
2021-06-24 21:01:06 +0200

    a wave of concentration

--- concentration.sh
+++ concentration.sh
@@ -1,44 +1,52 @@
+CONCENTRATION_MANTRA=(
+  conCENTRATE
+  ConcENTRATE
+  CONcenTRAtE
+  CONCEntrATE
+  CONCENtratE
+  CONCENTrate
+  CONCENtratE
+  CONCEntrATE
+  CONcenTRAtE
+  ConcENTRATE
+)
+CONCENTRATION_CMD='cal'
 CCCCount=0
-MAX_CCCs=8
+MAX_CCCs=50
+CCCCount_direction=1
+
 function concentration {
   EXIT_CODE=${1:-$?}
   if [ $EXIT_CODE -eq 130 ]; then # it was a Control-C
     # so increment the CCCCount
     echo concentrate
-    (( CCCCount++ ))
+    (( CCCCount = CCCCount + CCCCount_direction ))
     # and check
     if [ $CCCCount -ge $MAX_CCCs ]; then
-      AND_AGAIN_AND_AGAIN=$((CCCCount/MAX_CCCs))
-
-      if [ $AND_AGAIN_AND_AGAIN -ge 13 ]; then
-        AND_PRINT_CAL='cal'
-      fi
-
-      while [ $AND_AGAIN_AND_AGAIN -ge 2 ]; do
-        if [ $AND_AGAIN_AND_AGAIN -ge 3 ]; then
-          echo ConCENtraTe
-          if [ $AND_AGAIN_AND_AGAIN -ge 5 ]; then
-            echo CONcenTRATE
-            if [ $AND_AGAIN_AND_AGAIN -ge 8 ]; then
-              echo CONCENTRATE
-              if [ $AND_AGAIN_AND_AGAIN -ge 10 ]; then
-                echo CoNcEnTrAtE
-                if [ $AND_AGAIN_AND_AGAIN -ge 13 ]; then
-                  echo cOnCeNtRaTe
-                  (( CCCCount = CCCCount - 11*MAX_CCCs ))
-                  #concentration $EXIT_CODE
-                fi
-              fi
-            fi
-          fi
-        fi
-        (( AND_AGAIN_AND_AGAIN-- ))
-      done
-      $AND_PRINT_CAL
+      AND_CONCENTRATION_CMD=$CONCENTRATION_CMD
+      CCCCount_direction=-1
+      (( CCCCount-- ))
+    elif [ $CCCCount -le 0 ]; then
+      CCCCount_direction=1
+      (( CCCCount++ ))
     fi
+
+    NUM_MANTRAS=${#CONCENTRATION_MANTRA[@]}
+    CONCENTRATE_TIMES=1
+    if [ $CCCCount_direction -eq -1 ]; then
+      (( CONC_IDX_OFFSET = (CONC_IDX_OFFSET+2) % NUM_MANTRAS ))
+    else
+      CONC_IDX_OFFSET=0
+    fi
+    while [ $CONCENTRATE_TIMES -lt $CCCCount ]; do
+      CONC_IDX=$(( (CONCENTRATE_TIMES+CONC_IDX_OFFSET) % NUM_MANTRAS))
+      echo ${CONCENTRATION_MANTRA[$CONC_IDX]}
+      (( CONCENTRATE_TIMES++ ))
+    done
+    $AND_CONCENTRATION_CMD
   else
     CCCCount=0
-    AND_PRINT_CAL=
+    AND_CONCENTRATION_CMD=
   fi
 }
 case "$PROMPT_COMMAND" in
