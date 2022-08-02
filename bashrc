8d60a75de088c87dfdb6e133d2fafdfe1c9295a5
Mina Nagy Zaki <mnzaki@gmail.com>
2022-08-02 22:51:53 +0200

    feat(navigation): j to dir; jf to file

--- bashrc
+++ bashrc
@@ -1,7 +1,8 @@
 source /usr/share/mnzakish/lib/init.sh
 source "$MSH_DIR/concentration.sh"
 source "$MSH_DIR/intention.sh"
-#source "$MSH_DIR/categorization.sh"
+source "$MSH_DIR/navigation.sh"
+source "$MSH_DIR/categorization.sh"
 
 export PATH="$MSH_DIR/bin:$PATH"
 
---
+++ navigation.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: navigation.sh
+#
+#         USAGE: ./navigation.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: Nicolas Jar - Space is Only Noise - Too many kids finding rain in the dust
+#        AUTHOR: Mina Nagy Zaki (mnzaki), mnzaki@gmail.com
+#  ORGANIZATION: mnzaki.com
+#       CREATED: 01.08.2022 21:39:06
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                                  # Treat unset variables as an error
+
+NAVIGATION_LOOKAHEAD_DEPTH=7
+DIR_LIST_CMD="fd -t d -d $NAVIGATION_LOOKAHEAD_DEPTH"
+PATH_LIST_CMD="fd -t f -t l -d $NAVIGATION_LOOKAHEAD_DEPTH"
+
+# fzf completion
+source /usr/share/fzf/completion.bash
+# fzf is a fuzzy finder, with completion for bash on **<TAB>
+_fzf_compgen_path() {
+  echo "$1"
+  command $PATH_LIST_CMD "$1"
+}
+_fzf_compgen_dir() {
+  echo "$1"
+  command $DIR_LIST_CMD "$1"
+}
+
+# and this adds more of that support for some of my custom commands
+more_fzf_cmds="mpv mpvlowlat menow herenow zathura"
+for cmd in $more_fzf_cmds; do
+  __fzf_defc "$cmd" _fzf_path_completion "-o default -o bashdefault"
+done
+
+# super sayan jump to directory
+function j() {
+  local BASE="${1:-.}"
+  local JUMP_TO="$($DIR_LIST_CMD . "$BASE" | fzf)"
+  if [[ "$JUMP_TO" != "" ]]; then
+    pushd "$JUMP_TO"
+    echo -b $JUMP_TO | xsel -b
+  fi
+}
+
+# jump to file and run CMD
+function jf() {
+  local TARGET_CMD="${@-"xdg-open"}"
+  #if [[ "$TARGET_CMD" == "" ]]; then
+    #echo "Usage: jf CMD"
+    #echo "fuzzy find a file and run CMD on it"
+  #fi
+  set -x
+  local JUMP_TO="$($PATH_LIST_CMD . | fzf)"
+  set +x
+  if [[ "$JUMP_TO" != "" ]]; then
+    set -x
+    $TARGET_CMD "$JUMP_TO"
+    set +x
+  fi
+  echo -b $JUMP_TO | xsel -b
+}
