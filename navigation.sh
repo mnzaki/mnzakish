fbd13d9c9e8ba6835c0ee1898560d21d9b177f1c
Mina Nagy Zaki <mnzaki@gmail.com>
2022-11-03 02:33:06 +0100

    feat(navigation): j and jf history

--- navigation.sh
+++ navigation.sh
@@ -41,25 +41,29 @@ for cmd in $more_fzf_cmds; do
   __fzf_defc "$cmd" _fzf_path_completion "-o default -o bashdefault"
 done
 
+[ -e "$MSH_CACHE/j_hist" ] || touch "$MSH_CACHE/j_hist"
 # super sayan jump to directory
 function j() {
+  local FZF_HIST="$MSH_CACHE/j_hist"
   local BASE="${1:-.}"
-  local JUMP_TO="$($DIR_LIST_CMD . "$BASE" | fzf)"
+  local JUMP_TO="$($DIR_LIST_CMD . "$BASE" | fzf --history="$FZF_HIST")"
   if [[ "$JUMP_TO" != "" ]]; then
     pushd "$JUMP_TO"
     echo -b $JUMP_TO | xsel -b
   fi
 }
 
+[ -e "$MSH_CACHE/jf_hist" ] || touch "$MSH_CACHE/jf_hist"
 # jump to file and run CMD
 function jf() {
+  local FZF_HIST="$MSH_CACHE/jf_hist"
   local TARGET_CMD="${@-"xdg-open"}"
   #if [[ "$TARGET_CMD" == "" ]]; then
     #echo "Usage: jf CMD"
     #echo "fuzzy find a file and run CMD on it"
   #fi
   set -x
-  local JUMP_TO="$($PATH_LIST_CMD . | fzf)"
+  local JUMP_TO="$($PATH_LIST_CMD . | fzf --history="$FZF_HIST")"
   set +x
   if [[ "$JUMP_TO" != "" ]]; then
     set -x
