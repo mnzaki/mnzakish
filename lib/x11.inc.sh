1fe54aa33e00ff50404ff028e90552adb0654328
Mina Nagy Zaki <mnzaki@gmail.com>
2023-03-11 18:03:33 +0100

    fix(lib/x11): syntax

--- lib/x11.inc.sh
+++ lib/x11.inc.sh
@@ -23,46 +23,46 @@ source "$_msh_base/lib/fndispatch.inc.sh"
 # Focus an x11 window owned by a process with the given command line
 # or execute the command line if no such window
 function _msh_focus_or_exec() {
-  if [ "$1" = "" ]; then
+  #debug echo _msh_focus_or_exec "$@"
+  if [ -z "$1" ]; then
     return 1
   fi
-  if ! msh_focus_window_cmd "$@"; then
+  if ! _msh_focus_window_cmd "$@"; then
     $@
   fi
 }
 
 # Focus an x11 window owned by a process with the given command line
 function _msh_focus_window_cmd() {
+  #debug echo _msh_focus_window_cmd "$@"
   local cmd="$@"
-  if [ "$cmd" = "" ]; then
+  if [ -z "$cmd" ]; then
     return 1
   fi
 
-  local ps_path=$(which ps) # avoid aliases
-  echo "grepping for $cmd"
-  local pids="$($ps_path -eo pid,cmd | grep -F "$cmd" | grep -v grep | awk '{print $1}')"
-  if [ "$pids" = "" ]; then
+  #debug echo look for $cmd
+  local pids="$(command pgrep -f "$cmd")"
+  if [ -z "$pids" ]; then
     return 1
   else
-    msh_focus_window_pids $pids
+    _msh_focus_window_pids $pids
     return 0
   fi
 }
 
 # Focus x11 windows owned by a process with the given PIDs
 function _msh_focus_window_pids() {
-  if [ "$@" = "" ]; then
+  #debug _msh_focus_window_pids
+  if [ ${#@} -eq 0 ]; then
     return 1
   fi
   local pid wid windowids
   for pid in "$@"; do
     windowids=($(xdotool search --pid $pid))
-    if [ "$windowids" = "" ]; then
-      break
-    fi
 
     for wid in "${windowids[@]}"; do
       xdotool set_window --urgency 1 $wid
+      xdotool windowactivate $wid
     done
   done
   return 0
